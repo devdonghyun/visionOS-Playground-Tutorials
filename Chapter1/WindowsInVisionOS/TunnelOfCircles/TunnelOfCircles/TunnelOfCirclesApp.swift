f1edec17083b86dd702e0f99feb55f981a953b1f
Donghyun An <ehdgus8725@naver.com>
2026-05-06 00:11:51 +0900

    Add Tutorial 01 project

---
+++ Chapter1/WindowsInVisionOS/TunnelOfCircles/TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by 안동현 on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
