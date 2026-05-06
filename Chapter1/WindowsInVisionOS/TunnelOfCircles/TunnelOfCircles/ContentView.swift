07df758d0e7f0392cd8b87ec08ff55b394b28d26
Donghyun An <ehdgus8725@naver.com>
2026-05-07 02:08:27 +0900

    Add Depth to my 2d App

--- Chapter1/WindowsInVisionOS/TunnelOfCircles/TunnelOfCircles/ContentView.swift
+++ Chapter1/WindowsInVisionOS/TunnelOfCircles/TunnelOfCircles/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colors: [Color] = [.cyan, .blue]
+    @State private var depth = 0.0
     
     let minDiameter = 50.0
     let diameterChange = 70.0
@@ -22,9 +23,17 @@ struct ContentView: View {
                         .stroke(lineWidth: 30)
                         .foregroundStyle(colors[index % 2])
                         .frame(height: minDiameter + diameterChange * Double(index))
+                        .padding3D(.back, depth)
                 }
             }
             Grid {
+                GridRow {
+                    Text("Depth")
+                    Slider(value: $depth, in: 0...50) {
+                        Text("Depth")
+                    }
+                    
+                }
                 GridRow {
                     Text("Colors")
                     HStack {
