e4b2cace6eb95552417d75926ad8cf33e209c8c1
Donghyun An <ehdgus8725@naver.com>
2026-05-12 01:43:52 +0900

    Add an Ornament

--- Chapter2/LabelMaker/LabelMaker/ContentView.swift
+++ Chapter2/LabelMaker/LabelMaker/ContentView.swift
@@ -15,6 +15,15 @@ struct ContentView: View {
     var body: some View {
         LabelView(label: $label)
             .padding()
+            .ornament(attachmentAnchor: .scene(.bottom)) {
+                HStack(spacing: 30) {
+                    Slider(value: $label.cornerRadius, in: 0...100)
+                        .frame(width: 100)
+                }
+                .padding()
+                .glassBackgroundEffect()
+            }
+            
     }
 }
 
