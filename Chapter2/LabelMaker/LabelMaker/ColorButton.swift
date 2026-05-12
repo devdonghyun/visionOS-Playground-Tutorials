7750fd5810f7c18bda5882ab5c2f6b6a3b127daa
Donghyun An <ehdgus8725@naver.com>
2026-05-13 02:10:09 +0900

    Create Custom Color Control

---
+++ Chapter2/LabelMaker/LabelMaker/ColorButton.swift
@@ -0,0 +1,32 @@
+//
+//  ColorButton.swift
+//  LabelMaker
+//
+//  Created by 안동현 on 5/13/26.
+//
+
+import SwiftUI
+
+
+struct ColorButton: View {
+    @State var color: Color
+    var selectColor: (() -> Void)
+    
+    var body: some View {
+        Button {
+            selectColor()
+        } label: {
+            Circle()
+                .foregroundStyle(color)
+                .frame(height: 34)
+        }
+        .buttonBorderShape(.circle)
+    }
+}
+
+
+#Preview {
+    ColorButton(color: .cyan) {
+        
+    }
+}
